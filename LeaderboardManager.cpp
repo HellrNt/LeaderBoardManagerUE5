dddda0ee43181eab312ea37e9081db5af77f2a20
HellrNt <132611335+HellrNt@users.noreply.github.com>
2025-05-25 11:52:38 +0500

    Add files via upload

---
+++ LeaderboardManager.cpp
@@ -0,0 +1,405 @@
+#include "LeaderboardManager.h"
+#include "OnlineSubsystem.h"
+#include "OnlineStats.h"
+#include "Kismet/GameplayStatics.h"
+#include "Engine/World.h"
+
+void ULeaderboardManager::Initialize(UDataTable* InTable)
+{
+    LeaderboardMappingTable = InTable;
+    LeaderboardEntries.Empty();
+
+    IOnlineSubsystem* Subsystem = IOnlineSubsystem::Get();
+    if (Subsystem)
+    {
+        FString SubsystemName = Subsystem->GetSubsystemName().ToString();
+        if (SubsystemName == "EOS" || SubsystemName == "Epic")
+        {
+            PlatformType = ELeaderboardPlatform::Epic;
+        }
+        else
+        {
+            PlatformType = ELeaderboardPlatform::Steam;
+        }
+    }
+}
+
+void ULeaderboardManager::WriteToLeaderboard(const FString& WorldName, const FString& LeaderboardName, int32 Score)
+{
+    switch (PlatformType)
+    {
+        case ELeaderboardPlatform::Steam:
+        {
+            WriteToSteamLeaderboard(WorldName, LeaderboardName, Score);
+            break;
+        }
+        case ELeaderboardPlatform::Epic:
+        {
+            WriteToEpicLeaderboard(WorldName, LeaderboardName, Score);
+            break;
+        }
+    }
+}
+
+void ULeaderboardManager::ReadLeaderboard(const FString& WorldName, const FString& LeaderboardName, bool bFriendsOnly, int32 RankFirst, int32 RankCount, bool DoNotShowWindow)
+{
+    switch (PlatformType)
+    {
+        case ELeaderboardPlatform::Steam:
+        {
+            ReadFromSteamLeaderboard(WorldName, LeaderboardName, bFriendsOnly, RankFirst, RankCount, DoNotShowWindow);
+            break;
+        }
+        case ELeaderboardPlatform::Epic:
+        {
+            ReadFromEpicLeaderboard(WorldName, LeaderboardName, bFriendsOnly, RankFirst, RankCount, DoNotShowWindow);
+            break;
+        }
+    }
+}
+
+const TMap<FString, TArray<FLeaderboardEntry>>& ULeaderboardManager::GetLeaderboardEntries() const
+{
+    return LeaderboardEntries;
+}
+
+const TArray<FLeaderboardEntry>& ULeaderboardManager::GetLeaderboardByName(const FString& LeaderboardName) const
+{
+    if (const TArray<FLeaderboardEntry>* Found = LeaderboardEntries.Find(LeaderboardName))
+    {
+        return *Found;
+    }
+
+    static const TArray<FLeaderboardEntry> EmptyArray;
+    return EmptyArray;
+}
+
+void ULeaderboardManager::OnLeaderboardReadComplete(bool bWasSuccessful, FOnlineLeaderboardReadRef LeaderboardReadRef)
+{
+    IOnlineSubsystem* Subsystem = IOnlineSubsystem::Get();
+    if (Subsystem)
+    {
+        IOnlineLeaderboardsPtr Leaderboards = Subsystem->GetLeaderboardsInterface();
+        if (bWasSuccessful)
+        {
+            if (LeaderboardEntries.Contains(LeaderboardReadRef->LeaderboardName.ToString()))
+            {
+                LeaderboardEntries[LeaderboardReadRef->LeaderboardName.ToString()].Empty();
+            }
+            int32 MyRank = -1;
+            TSharedPtr<const FUniqueNetId> UserId;
+            IOnlineIdentityPtr Identity = Subsystem->GetIdentityInterface();
+            if (Identity.IsValid())
+            {
+                UserId = Identity->GetUniquePlayerId(0);
+            }
+            UE_LOG(LogTemp, Log, TEXT("Leaderboard data successfully read."));
+
+            for (const FOnlineStatsRow& Row : LeaderboardReadRef->Rows)
+            {
+                FString LocalPlayerName = Row.NickName;
+                int32 PlayerScore = 0;
+
+                const FVariantData* ScoreData = Row.Columns.Find(LeaderboardReadRef->SortedColumn);
+                if (ScoreData)
+                {
+                    ScoreData->GetValue(PlayerScore);
+                }
+                FLeaderboardEntry NewEntry;
+                NewEntry.PlayerName = LocalPlayerName;
+                NewEntry.Score = PlayerScore;
+                NewEntry.Rank = Row.Rank;
+                LeaderboardEntries.FindOrAdd(LeaderboardReadRef->LeaderboardName.ToString()).Add(NewEntry);
+                if (MyRank == -1 && UserId.IsValid() && Row.PlayerId == UserId)
+                {
+                    MyRank = Row.Rank;
+                }
+                UE_LOG(LogTemp, Log, TEXT("Player: %s, Score: %d"), *LocalPlayerName, PlayerScore);
+            }
+
+            if (LeaderboardEntries.Contains(LeaderboardReadRef->LeaderboardName.ToString()) && LeaderboardEntries[LeaderboardReadRef->LeaderboardName.ToString()].Num() > 1)
+            {
+                LeaderboardEntries[LeaderboardReadRef->LeaderboardName.ToString()].Sort([](const FLeaderboardEntry& A, const FLeaderboardEntry& B)
+                {
+                    return A.Rank < B.Rank;
+                });
+
+                if (LeaderboardEntries[LeaderboardReadRef->LeaderboardName.ToString()].Num() >= 50)
+                {
+                    if (MyRank <= 1)
+                    {
+                        OnLeaderboardAchievment.Broadcast("LeaderboardLegend");
+                    }
+                }
+            }
+            if (!bIsDoNotShowWindow)
+            {
+                OnLeaderboardWindowShow.Broadcast(bIsFriendLeaderboard);
+            }
+
+            OnLeaderBoardQueryCompleted.Broadcast(NAME_None, true);
+        }
+        else
+        {
+            if (LeaderboardEntries.Contains(LeaderboardReadRef->LeaderboardName.ToString()))
+            {
+                LeaderboardEntries[LeaderboardReadRef->LeaderboardName.ToString()].Empty();
+            }
+            if (!bIsDoNotShowWindow)
+            {
+                OnLeaderboardWindowShow.Broadcast(bIsFriendLeaderboard);
+            }
+            UE_LOG(LogTemp, Warning, TEXT("Failed to read leaderboard data."));
+            OnLeaderBoardQueryCompleted.Broadcast(NAME_None, false);
+        }
+        if (Leaderboards)
+        {
+            Leaderboards->ClearOnLeaderboardReadCompleteDelegate_Handle(QueryLeaderboardDelegateHandle);
+        }
+    }
+}
+
+void ULeaderboardManager::OnLeaderboardFlushComplete(FName SessionName, bool bWasSuccessful)
+{
+    IOnlineSubsystem* Subsystem = IOnlineSubsystem::Get();
+    if (Subsystem)
+    {
+        IOnlineLeaderboardsPtr Leaderboards = Subsystem->GetLeaderboardsInterface();
+        if (Leaderboards.IsValid())
+        {
+            Leaderboards->ClearOnLeaderboardFlushCompleteDelegate_Handle(FlushLeaderboardDelegateHandle);
+            FlushLeaderboardDelegateHandle.Reset();
+        }
+    }
+
+    UE_LOG(LogTemp, Log, TEXT("Leaderboard flush %s for session: %s"),
+        bWasSuccessful ? TEXT("succeeded") : TEXT("failed"),
+        *SessionName.ToString());
+
+    OnLeaderBoardFlushCompleted.Broadcast(SessionName, bWasSuccessful);
+}
+
+// ===== Steam реализация =====
+
+void ULeaderboardManager::WriteToSteamLeaderboard(const FString& WorldName, const FString& LeaderboardName, int32 Score)
+{
+    IOnlineSubsystem* OnlineSub = IOnlineSubsystem::Get();
+    if (OnlineSub)
+    {
+        IOnlineIdentityPtr IdentityInterface = OnlineSub->GetIdentityInterface();
+        if (IdentityInterface.IsValid())
+        {
+            FUniqueNetIdPtr UserId = IdentityInterface->GetUniquePlayerId(0);
+            if (UserId.IsValid())
+            {
+                IOnlineLeaderboardsPtr Leaderboards = OnlineSub->GetLeaderboardsInterface();
+                if (Leaderboards.IsValid())
+                {
+                    FString LeaderboardAPIName, StatName;
+                    GetMappedLeaderboardAndStat(LeaderboardName, LeaderboardAPIName, StatName);
+                    FOnlineLeaderboardWrite WriteObject;
+                    WriteObject.LeaderboardNames.Add(FName(*LeaderboardName));
+                    WriteObject.RatedStat = FName(*LeaderboardAPIName);
+                    WriteObject.SortMethod = ELeaderboardSort::Descending;
+                    WriteObject.UpdateMethod = ELeaderboardUpdateMethod::KeepBest;
+                    WriteObject.SetIntStat(FName(*StatName), Score);
+
+                    if (Leaderboards->WriteLeaderboards(FName(WorldName), *UserId, WriteObject))
+                    {
+                        FlushLeaderboardDelegateHandle = Leaderboards->AddOnLeaderboardFlushCompleteDelegate_Handle(
+                            FOnLeaderboardFlushCompleteDelegate::CreateUObject(this, &ULeaderboardManager::OnLeaderboardFlushComplete));
+                        Leaderboards->FlushLeaderboards(FName(WorldName));
+                    }
+                }
+            }
+            else
+            {
+                UE_LOG(LogTemp, Warning, TEXT("Failed to get UserId."));
+            }
+        }
+        else
+        {
+            UE_LOG(LogTemp, Warning, TEXT("IdentityInterface is not valid."));
+        }
+    }
+    else
+    {
+        UE_LOG(LogTemp, Warning, TEXT("OnlineSubsystem is not available."));
+    }
+}
+
+void ULeaderboardManager::ReadFromSteamLeaderboard(const FString& WorldName, const FString& LeaderboardName, bool bFriendsOnly, int32 RankFirst, int32 RankCount, bool DoNotShowWindow)
+{
+    if (IOnlineSubsystem* Subsystem = IOnlineSubsystem::Get())
+    {
+        IOnlineIdentityPtr Identity = Subsystem->GetIdentityInterface();
+        if (Identity.IsValid())
+        {
+            FUniqueNetIdPtr NetId = Identity->GetUniquePlayerId(0);
+            if (!NetId)
+            {
+                return;
+            }
+        }
+
+        IOnlineLeaderboardsPtr Leaderboards = Subsystem->GetLeaderboardsInterface();
+        if (Leaderboards.IsValid())
+        {
+            FString LeaderboardAPIName, StatName;
+            GetMappedLeaderboardAndStat(LeaderboardName, LeaderboardAPIName, StatName);
+            bIsDoNotShowWindow = DoNotShowWindow;
+            FOnlineLeaderboardReadRef LeaderboardReadRef = MakeShared<FOnlineLeaderboardRead>();
+            LeaderboardReadRef->LeaderboardName = FName(*LeaderboardAPIName);
+            LeaderboardReadRef->SortedColumn = FName(*StatName);
+            FColumnMetaData ColumnMetaData = FColumnMetaData(FName(*StatName), EOnlineKeyValuePairDataType::Int32);
+            LeaderboardReadRef->ColumnMetadata.Add(ColumnMetaData);
+            LeaderboardReadRef->Rows.Empty();
+
+            // Привязываем делегат
+            QueryLeaderboardDelegateHandle =
+                Leaderboards->AddOnLeaderboardReadCompleteDelegate_Handle(FOnLeaderboardReadCompleteDelegate::CreateUObject(
+                    this,
+                    &ULeaderboardManager::OnLeaderboardReadComplete, LeaderboardReadRef));
+
+            bIsFriendLeaderboard = bFriendsOnly;
+            if (bFriendsOnly)
+            {
+                if (!Leaderboards->ReadLeaderboardsForFriends(0, LeaderboardReadRef))
+                {
+                    UE_LOG(LogTemp, Error, TEXT("Failed to read friends leaderboard."));
+                    Leaderboards->ClearOnLeaderboardReadCompleteDelegate_Handle(QueryLeaderboardDelegateHandle);
+                    QueryLeaderboardDelegateHandle.Reset();
+                }
+            }
+            else
+            {
+                if (!Leaderboards->ReadLeaderboardsAroundRank(RankFirst, RankCount, LeaderboardReadRef))
+                {
+                    UE_LOG(LogTemp, Error, TEXT("Failed to read global leaderboard."));
+                    Leaderboards->ClearOnLeaderboardReadCompleteDelegate_Handle(QueryLeaderboardDelegateHandle);
+                    QueryLeaderboardDelegateHandle.Reset();
+                }
+            }
+        }
+    }
+}
+
+// ===== Epic реализация =====
+
+void ULeaderboardManager::GetMappedLeaderboardAndStat(const FString& DisplayName, FString& OutLeaderboardName, FString& OutStatName)
+{
+    if (!LeaderboardMappingTable) return;
+
+    const FLeaderboardPlatformMappingRow* Mapping = LeaderboardMappingTable->FindRow<FLeaderboardPlatformMappingRow>(*DisplayName, TEXT(""));
+    if (!Mapping) return;
+
+    switch (PlatformType)
+    {
+        case ELeaderboardPlatform::Steam:
+        {
+            OutLeaderboardName = Mapping->SteamLeaderboardName;
+            OutStatName = Mapping->SteamStatName;
+            break;
+
+        }
+        case ELeaderboardPlatform::Epic:
+        {
+            OutLeaderboardName = Mapping->EpicLeaderboardName;
+            OutStatName = Mapping->EpicStatName;
+            break;
+        }
+    }
+}
+
+void ULeaderboardManager::WriteToEpicLeaderboard(const FString& WorldName, const FString& LeaderboardName, int32 Score)
+{
+    if (IOnlineSubsystem* Subsystem = IOnlineSubsystem::Get())
+    {
+        IOnlineStatsPtr StatsInterface = Subsystem->GetStatsInterface();
+        if (StatsInterface.IsValid())
+        {
+            IOnlineIdentityPtr Identity = Subsystem->GetIdentityInterface();
+            if (Identity.IsValid())
+            {
+                FUniqueNetIdPtr UserId = Identity->GetUniquePlayerId(0);
+                if (UserId.IsValid())
+                {
+                    IOnlineLeaderboardsPtr Leaderboards = Subsystem->GetLeaderboardsInterface();
+                    if (Leaderboards.IsValid())
+                    {
+                        FString LeaderboardAPIName, StatName;
+                        GetMappedLeaderboardAndStat(LeaderboardName, LeaderboardAPIName, StatName);
+                        FOnlineLeaderboardWrite WriteObject;
+                        WriteObject.LeaderboardNames.Add(FName(*LeaderboardAPIName));
+                        WriteObject.RatedStat = FName(*StatName);
+                        WriteObject.SortMethod = ELeaderboardSort::Descending;
+                        WriteObject.UpdateMethod = ELeaderboardUpdateMethod::KeepBest;
+                        WriteObject.SetIntStat(FName(*StatName), Score);
+
+                        if (Leaderboards->WriteLeaderboards(FName(WorldName), *UserId, WriteObject))
+                        {
+                            FlushLeaderboardDelegateHandle = Leaderboards->AddOnLeaderboardFlushCompleteDelegate_Handle(
+                                FOnLeaderboardFlushCompleteDelegate::CreateUObject(this, &ULeaderboardManager::OnLeaderboardFlushComplete));
+                            Leaderboards->FlushLeaderboards(FName(WorldName));
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
+
+void ULeaderboardManager::ReadFromEpicLeaderboard(const FString& WorldName, const FString& LeaderboardName, bool bFriendsOnly, int32 RankFirst, int32 RankCount, bool DoNotShowWindow)
+{
+    IOnlineSubsystem* Subsystem = IOnlineSubsystem::Get();
+    if (Subsystem)
+    {
+        IOnlineIdentityPtr Identity = Subsystem->GetIdentityInterface();
+        if (Identity.IsValid())
+        {
+            FUniqueNetIdPtr NetId = Identity->GetUniquePlayerId(0);
+            if (!NetId || Identity->GetLoginStatus(*NetId) != ELoginStatus::LoggedIn)
+            {
+                return;
+            }
+        }
+
+        IOnlineLeaderboardsPtr Leaderboards = Subsystem->GetLeaderboardsInterface();
+        if (Leaderboards.IsValid())
+        {
+            FString LeaderboardAPIName, StatName;
+            GetMappedLeaderboardAndStat(LeaderboardName, LeaderboardAPIName, StatName);
+            bIsDoNotShowWindow = DoNotShowWindow;
+            FOnlineLeaderboardReadRef LeaderboardReadRef = MakeShared<FOnlineLeaderboardRead, ESPMode::ThreadSafe>();
+            LeaderboardReadRef->LeaderboardName = FName(*LeaderboardAPIName);
+            LeaderboardReadRef->SortedColumn = FName(*StatName);
+            FColumnMetaData ColumnMetaData = FColumnMetaData(FName(*StatName), EOnlineKeyValuePairDataType::Int32);
+            LeaderboardReadRef->ColumnMetadata.Add(ColumnMetaData);
+            // Привязываем делегат
+            QueryLeaderboardDelegateHandle =
+                Leaderboards->AddOnLeaderboardReadCompleteDelegate_Handle(FOnLeaderboardReadCompleteDelegate::CreateUObject(
+                    this,
+                    &ULeaderboardManager::OnLeaderboardReadComplete, LeaderboardReadRef));
+
+            bIsFriendLeaderboard = bFriendsOnly;
+            if (bFriendsOnly)
+            {
+                if (!Leaderboards->ReadLeaderboardsForFriends(0, LeaderboardReadRef))
+                {
+                    UE_LOG(LogTemp, Error, TEXT("Failed to read friends leaderboard."));
+                    Leaderboards->ClearOnLeaderboardReadCompleteDelegate_Handle(QueryLeaderboardDelegateHandle);
+                    QueryLeaderboardDelegateHandle.Reset();
+                }
+            }
+            else
+            {
+                if (!Leaderboards->ReadLeaderboardsAroundRank(RankFirst, RankCount, LeaderboardReadRef))
+                {
+                    UE_LOG(LogTemp, Error, TEXT("Failed to read global leaderboard."));
+                    Leaderboards->ClearOnLeaderboardReadCompleteDelegate_Handle(QueryLeaderboardDelegateHandle);
+                    QueryLeaderboardDelegateHandle.Reset();
+                }
+            }
+        }
+    }
+}
---
+++ LeaderboardManager.h
@@ -0,0 +1,114 @@
+#pragma once
+
+#include "CoreMinimal.h"
+#include "UObject/NoExportTypes.h"
+#include "OnlineError.h"
+#include "OnlineSubsystem.h"
+#include "Interfaces/OnlineLeaderboardInterface.h"
+#include "Interfaces/OnlineStatsInterface.h"
+#include "Interfaces/OnlineIdentityInterface.h"
+#include "LeaderboardManager.generated.h"
+
+DECLARE_DYNAMIC_MULTICAST_DELEGATE_TwoParams(FOnLeaderBoardFlushCompleted, FName, SessionName, bool, bWasSuccessful);
+DECLARE_DYNAMIC_MULTICAST_DELEGATE_TwoParams(FOnLeaderBoardQueryCompleted, FName, SessionName, bool, bWasSuccessful);
+DECLARE_MULTICAST_DELEGATE_OneParam(FOnLeaderboardWindowShow, bool);
+DECLARE_MULTICAST_DELEGATE_OneParam(FOnLeaderboardAchievment, const FString&);
+
+UENUM(BlueprintType)
+enum class ELeaderboardPlatform : uint8
+{
+    Steam,
+    Epic
+};
+
+USTRUCT(BlueprintType)
+struct FLeaderboardPlatformMappingRow : public FTableRowBase
+{
+    GENERATED_BODY();
+
+    UPROPERTY(EditAnywhere, BlueprintReadWrite)
+    FString LeaderboardDisplayName;
+
+    UPROPERTY(EditAnywhere, BlueprintReadWrite)
+    FString SteamLeaderboardName;
+
+    UPROPERTY(EditAnywhere, BlueprintReadWrite)
+    FString SteamStatName;
+
+    UPROPERTY(EditAnywhere, BlueprintReadWrite)
+    FString EpicLeaderboardName;
+
+    UPROPERTY(EditAnywhere, BlueprintReadWrite)
+    FString EpicStatName;
+};
+
+USTRUCT(BlueprintType)
+struct FLeaderboardEntry
+{
+    GENERATED_BODY()
+
+    UPROPERTY(EditAnywhere, BlueprintReadWrite)
+    FString PlayerName;
+
+    UPROPERTY(EditAnywhere, BlueprintReadWrite)
+    int32 Score;
+
+    UPROPERTY(EditAnywhere, BlueprintReadWrite)
+    int32 Rank;
+
+    FLeaderboardEntry()
+        : PlayerName(TEXT("Unknown")), Score(0), Rank(0) {}
+};
+
+UCLASS()
+class YOUR_GAME_API ULeaderboardManager : public UObject
+{
+    GENERATED_BODY()
+
+public:
+    void Initialize(UDataTable* InTable);
+
+    UFUNCTION(BlueprintCallable, Category = "Leaderboard")
+    void WriteToLeaderboard(const FString& WorldName, const FString& LeaderboardName, int32 Score);
+    
+    UFUNCTION(BlueprintCallable, Category = "Leaderboard")
+    void ReadLeaderboard(const FString& WorldName, const FString& LeaderboardName,  bool bFriendsOnly, int32 RankFirst, int32 RankCount, bool DoNotShowWindow = false);
+
+    UFUNCTION(BlueprintCallable, Category = "Leaderboard")
+    const TArray<FLeaderboardEntry>& GetLeaderboardByName(const FString& LeaderboardName) const;
+
+    UFUNCTION(BlueprintCallable, Category = "Leaderboard")
+    void GetMappedLeaderboardAndStat(const FString& DisplayName, FString& OutLeaderboardName, FString& OutStatName);
+
+    const TMap<FString, TArray<FLeaderboardEntry>>& GetLeaderboardEntries() const;
+
+    FOnLeaderboardWindowShow OnLeaderboardWindowShow;
+    FOnLeaderboardAchievment OnLeaderboardAchievment;
+
+    UPROPERTY(BlueprintAssignable, Category = "Leaderboard")
+    FOnLeaderBoardFlushCompleted OnLeaderBoardFlushCompleted;
+
+    UPROPERTY(BlueprintAssignable, Category = "Leaderboard")
+    FOnLeaderBoardQueryCompleted OnLeaderBoardQueryCompleted;
+
+private:
+    void WriteToSteamLeaderboard(const FString& WorldName, const FString& LeaderboardName, int32 Score);
+    void WriteToEpicLeaderboard(const FString& WorldName, const FString& LeaderboardName, int32 Score);
+    void ReadFromSteamLeaderboard(const FString& WorldName, const FString& LeaderboardName, bool bFriendsOnly, int32 RankFirst, int32 RankCount, bool DoNotShowWindow = false);
+    void ReadFromEpicLeaderboard(const FString& WorldName, const FString& LeaderboardName, bool bFriendsOnly, int32 RankFirst, int32 RankCount, bool DoNotShowWindow = false);
+
+    void OnLeaderboardReadComplete(bool bWasSuccessful, FOnlineLeaderboardReadRef LeaderboardReadRef);
+    void OnLeaderboardFlushComplete(FName SessionName, bool bWasSuccessful);
+
+    FDelegateHandle QueryLeaderboardDelegateHandle;
+    FDelegateHandle FlushLeaderboardDelegateHandle;
+
+    TMap<FString, TArray<FLeaderboardEntry>> LeaderboardEntries;
+
+    UPROPERTY()
+    UDataTable* LeaderboardMappingTable;
+
+    bool bIsFriendLeaderboard = false;
+    bool bIsDoNotShowWindow = false;
+    ELeaderboardPlatform PlatformType = ELeaderboardPlatform::Steam;
+};
